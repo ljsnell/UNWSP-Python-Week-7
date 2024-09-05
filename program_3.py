a3746685da4e6430b17471d9d070f632ffedece3
ljsnell <ljsnell7@gmail.com>
2024-09-04 21:49:02 -0500

    Unit tests for test_program 2

---
+++ program_3.py
@@ -0,0 +1,8 @@
+# Program #3: US_Population
+# Have the user input (using a loop) various information that contains three pieces of data: 
+# year, name of state, and population.  
+# Store all of this information in a list of lists.  For example it might be stored like this:
+
+# [[2010, "Maine", 1987435], [2010,"Minnesota",6873202], [2011, "Iowa", 3421988]]
+
+# Now have the user enter a year.  The program will add the populations from all states in the list of list for that year only.   
