0378d99d6e80fe74d5fbda77d4735542f5c4d1bb
ljsnell <ljsnell7@gmail.com>
2024-09-04 22:24:49 -0500

    Updated to unit test program 3 as well.

--- program_2.py
+++ program_2.py
@@ -29,7 +29,7 @@ def main():
 # in the list that are greater than the number.
 def display_larger_than_n_list(n, n_list):
     # Write your code to display all of the numbers in the list that are greater than then number n. below
-    
+    print('In display_larger_than_n_list')
         
 # Call the main function.
 if __name__ == '__main__':
